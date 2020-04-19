8ccc75e6b89f7b62373c9b7b407712425357fb3b
Sejal Dua <sejal.dua@tufts.edu>
2020-04-19 01:32:40 -0700

    functionality for modals

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // default search query
     var q = "chicken";
+    var state = 0;
     requestData(q);
     
     function requestData(q) {
@@ -12,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
         request.open('GET', `https://www.themealdb.com/api/json/v1/1/filter.php?i=`+q, true);
         console.log("2: opened request file");
         // Step 3: set up event handler / callback
-        request.onreadystatechange = function() {
+        request.onload = function() {
             console.log("3: readystatechange event fired");
     
             if (request.readyState == 4 && request.status == 200) {
@@ -37,20 +38,20 @@ document.addEventListener('DOMContentLoaded', function () {
                         }
                         // alternating color scheme
                         if (i % 2 == 0)
-                            s += "<th class='title'>" + data[x].strMeal + "</th>";
+                            s += "<th class='button' data-modal='modal" + i + "'>" + data[x].strMeal + "</th>";
                         else
-                            s += "<th style='background-color:#27252B' class='title'>" + data[x].strMeal + "</th>";
+                            s += "<th style='background-color:#27252B' class='button' data-modal='modal" + i + "'>" + data[x].strMeal + "</th>";
                         s += "<th><img src='" + data[x].strMealThumb + "' width='100' height='100'></img></th>";
                         if (i == entries - 1)
                             s += "</tr>";
                         i += 1;
+                        document.getElementById("blooock").innerHTML += createModal(i, data[x].strMeal);
                     }
                     s += "</table";
 
-                    console.log('ID:', data);
-                    console.log(JSON.parse(request.responseText).data);
-                    
+                    console.log('ID:', data);                    
                     document.getElementById("results").innerHTML = s;
+                    listenClick();
                 }
             } 
             else if (request.readyState == 4 && request.status != 200) {
@@ -70,11 +71,26 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log("4: Request sent");
     }
 
+    function createModal(i, meal_name) {
+        console.log(i, meal_name);
+        var divstr = "";
+        divstr += "<div class='modal' id='modal"+i+"'>";
+        divstr += "<div class='modal-content' id='modal"+i+"content'>";
+        divstr += "<span class='close'>&times;</span>";
+        divstr + "<p>"+meal_name+"</p>";
+        divstr += "</div>";
+        divstr += "</div>";
+        return divstr;
+    }
+
     function generate() {
+        document.getElementById("blooock").innerHTML = "";
         q = document.getElementById("query").value;
         console.log('Query: ',document.getElementById("query").value);
         setTimeout(clear, 1000);
+        console.log("before");
         requestData(q);
+        console.log("after");
     }
 
     function clear() {
@@ -89,3 +105,39 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+function listenClick() {
+    console.log("testing");
+    var modalBtns = [...document.querySelectorAll(".button")];
+    modalBtns.forEach(function(btn){
+        console.log(btn);
+        btn.addEventListener("click", function() {
+            console.log("clicked");
+            var modal = btn.getAttribute('data-modal');
+            document.getElementById(modal).style.display = "block";
+            document.getElementById(modal+'content').innerHTML = "<span class='close'>&times;</span><p>"+modal+"</p>"
+        })
+    });
+    
+    var closeBtns = [...document.querySelectorAll(".close")];
+    closeBtns.forEach(function(btn){
+        btn.addEventListener("click", function() {
+            var modal = btn.closest('.modal');
+            modal.style.display = "none";
+        })
+    });
+    console.log("here");
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+         modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target.className === "modal") {
+            event.target.style.display = "none";
+        }
+    }
+}
