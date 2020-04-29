505cf3ebc6ff4587b6a2cfc364261262cab29d0c
henrycaron <57197754+henrycaron@users.noreply.github.com>
2020-04-29 12:59:42 -0400

    added get

--- mongo_access.js
+++ mongo_access.js
@@ -22,7 +22,7 @@ app.use(function(req,res,next){
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers','Content-Type');
     next();
-};
+});
 
 //Note that in version 4 of express, express.bodyParser() was
 //deprecated in favor of a separate 'body-parser' module.
